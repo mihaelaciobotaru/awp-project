5349c9edb1f32a3e810ee06fd0ba9d6cd52f7dbc
Mihaela <mihaela@MacBook-Pro.local>
2015-11-29 04:51:40 +0200

    Tema4

--- socialapp/forms.py
+++ socialapp/forms.py
@@ -5,3 +5,9 @@ class UserPostForm(Form):
     text = CharField(widget=Textarea(
         attrs={'cols': 100, 'rows': 5}),
         label="Enter your post here")
+
+
+class UserPostCommentForm(Form):
+    text = CharField(widget=Textarea(
+        attrs={'cols': 50, 'rows': 3}),
+        label="Enter your comment here")
--- socialapp/views.py
+++ socialapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from socialapp.models import UserPost
-from socialapp.forms import UserPostForm
+from socialapp.models import UserPost, UserPostComment
+from socialapp.forms import UserPostForm, UserPostCommentForm
 
 
 def index(request):
@@ -24,5 +24,20 @@ def index(request):
 
 def post_details(request, pk):
     post = UserPost.objects.get(pk=pk)
-    context = {'post': post}
-    return render(request, 'post_details.html', context)
+    if request.method == 'GET':
+        comments = UserPostComment.objects.filter(post_id=post).order_by('-date_added')
+        form = UserPostCommentForm()
+        context = {'post': post,
+                   'form': form,
+                   'comments': comments,
+                   }
+        return render(request, 'post_details.html', context)
+    elif request.method == 'POST':
+        form = UserPostCommentForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            user_post_comment = UserPostComment(text=text, post=post)
+            user_post_comment.save()
+        return redirect('post_details', pk=pk)
+
+
